52e86e84b4f6633d1e00e9226dfc9cd0454fdbc2
bazan ivan <ivanbazan12xd@gmail.com>
2022-11-25 14:29:12 -0300

    agregue la navbar

---
+++ mi-app/src/componentes/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <div>
+            <img src=''/>
+            <button>Tipo</button>
+            <button>Alfabetico</button>
+            <button>Legendarios</button>
+            <button>Generacion</button>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
